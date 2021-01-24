82dcddcd0d7ef07c2b3ed4ed02d328907e18e063
hebert felipe <hebertfelipe.97@outlook.com.br>
2021-01-24 15:22:28 -0300

    refactor: add change from getdate() to getMilisseconds on CalculationPrevision

--- src/utils/calculatePrevision.ts
+++ src/utils/calculatePrevision.ts
@@ -2,7 +2,7 @@ import { CalculatePrevision } from '../models/calculate-prevision'
 
 export class CalculatePrevisionClass implements CalculatePrevision {
     calculate(value: number): string {
-        const calc = Math.floor(Math.random() * (new Date().getDate())) + 24
+        const calc = Math.floor(Math.random() * (new Date().getMilliseconds())) + 24
         const response = `${value} -> ${calc}`
         return response
     }
